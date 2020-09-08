b8eecd7fcfc1d2058803214f4726204664d5c4b0
suna001 <nooneed@icloud.com>
2020-09-08 21:32:31 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+# Rails.application.routes.draw do
+#   get 'posts', to: 'posts#index'
+#   get 'posts/new', to: 'posts#new'
+# end
+
+Rails.application.routes.draw do
+  get 'posts', to: 'posts#index'
+  get 'posts/new', to: 'posts#new'
+  post 'posts', to: 'posts#create'
+end
